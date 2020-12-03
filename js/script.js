6de335f371ec912e55e59db57f3704298905fec2
Mariusz <mbielicki@wp.pl>
2020-12-03 02:04:33 +0100

    My first GIT/GITHUB repository

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("first").style.backgroundColor = "black";
+document.getElementById("first").style.color = "white";
+
+document.getElementById("second").style.backgroundColor = "red";
+document.getElementById("second").style.color = "white";
+
+document.getElementById("third").style.backgroundColor = "navy";
+document.getElementById("third").style.color = "orangered";
